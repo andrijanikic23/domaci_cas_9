8ae2dbde175dcfca89c0e77aabf3955a87512166
andrijanikic23 <andrijanikic001@gmail.com>
2025-03-19 10:14:03 +0100

    Homework number 13

--- app/Models/ForecastsModel.php
+++ app/Models/ForecastsModel.php
@@ -9,11 +9,13 @@ class ForecastsModel extends Model
     protected $table = 'forecasts';
 
     protected $fillable = [
-        'city_id', 'temperature', 'date'
+        'city_id', 'temperature', 'date', 'weather_type', 'probability'
     ];
+
+    const WEATHERS = ["rainy", "sunny", "snowy" ];
 
     public function cities()
     {
-        return $this->belongsTo(CitiesModel::class);
+        return $this->hasOne(CitiesModel::class, 'id', 'city_id');
     }
 }
